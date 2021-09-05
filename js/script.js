ef1a7b701e620108e786b32d6cb31f49c22f5454
Blake Eriks <eblake98@vt.edu>
2021-09-04 21:12:27 -0700

    Prepping for deploy, added search bar functionality

--- js/script.js
+++ js/script.js
@@ -1,34 +1,16 @@
-let matchData, matchList, leagueData;
+let completeMatchList, filteredMatchList, leagueData;
 
 let matchesShown = 5;
 
-const init = () => {
+const $matchList = $('.matchList');
+const $matchItem = $('.matchItem');
+const $searchInput = $('.searchInput');
 
-    // $.ajax({
-    //     url :'https://api-football-v1.p.rapidapi.com/v3/leagues',
-    //     data : {'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
-    //             'x-rapidapi-key': '8b4645bb76mshefe90b7960b30c0p107a50jsn3bef4a0b86fd'}
-    // }).then( data => {
-    //     console.log(data);
-    // }, error => {
-    //     console.log(error);
-    // })
-
-    $.get('data.json').then( data => {
-        matchData = preprocessMatchData(data.response);
-        matchList = matchData.slice(0,5);
-        renderMatches();
-    });
-
-    $.get('./static/leagues1.json').then( data => {
-        leagueData = data;
-    }).then( () => {
-        //console.log(leagueData);
-    });
-}
-
-const renderMatches = () => {
-    matchList.forEach(match => {
+const preprocessMatchData = matchData => {
+    let htmlMatchList = [];
+    for (match of matchData) {
+        match.title = match.title.replace('-', "vs");
+        match.competition = match.competition.substring(match.competition.indexOf(' '));
         let $matchItem = $('<div>').addClass('matchItem').css('height', $(this).height);
         let $matchTitle = $('<div>').addClass('matchTitle');
         let $matchTitleDetails = $('<div>').addClass('matchTitleDetails');
@@ -48,43 +30,56 @@ const renderMatches = () => {
         
         $matchItem.append($matchTitle);
         $matchItem.append($matchContent);
-        $matchList.append($matchItem);
-    });
+        htmlMatchList.push($matchItem);
+    }
+    return htmlMatchList;
+}
 
+const refreshCallbacks = () => {
+    const $showMoreButton = $('.showMoreButton');
+    $showMoreButton.on('click', () => {
+        matchesShown = (matchesShown + 5 > completeMatchList.length) ? completeMatchList.length : matchesShown + 5;
+        renderMatches();
+    });
+}
+
+const renderMatches = () => {
+    $matchList.empty();
+    for(let i = 0; i < matchesShown; i++) {
+        $matchList.append(filteredMatchList[i]);
+    }
     let $showMore = $('<div>').addClass('showMoreButton').text('Show more...');
     $matchList.append($showMore);
+    refreshCallbacks();
+}
+
+const init = () => {
+
+    $.ajax({
+        url: `https://www.scorebat.com/video-api/v3/`
+    }).then(
+        data => {
+            completeMatchList = preprocessMatchData(data.response);
+            filteredMatchList = completeMatchList;
+            renderMatches();
+        },
+        error => {
+            console.log(error);
+        }
+    );
+
+    $searchInput.on('input', () => {
+        let filter = $searchInput.val();
+        filteredMatchList = completeMatchList.filter( match => match.children('.matchTitle')[0].outerText.includes(filter));
+        renderMatches();
+    });
 }
 
 const formatDate = date => {
-    
     formattedDate = new Date(date);
     const month = formattedDate.getMonth() + 1;
     const day = formattedDate.getDate();
     return month + '/' + day;
 }
 
-const $matchList = $('.matchList');
-const $matchItem = $('.matchItem');
-const $showMoreButton = $('.showMoreButton');
-
 init();
-
-function preprocessMatchData(matchData) {
-    for (match of matchData) {
-        match.title = match.title.replace('-', "vs");
-        match.competition = match.competition.substring(match.competition.indexOf(' '));
-    }
-    return matchData;
-}
-
-// $.ajax({
-//     url: `https://www.scorebat.com/video-api/v3/`
-// }).then(
-//     data => {
-//         matchData = data;
-//         console.log(matchData);
-//     },
-//     error => {
-//         console.log(error);
-//     }
-// );
